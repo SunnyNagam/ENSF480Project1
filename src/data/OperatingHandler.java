40b7e41e697e89c3d7b69b970bba8f56e8b3c703
Keenan Gaudio <keenangaudio@me.com>
2018-11-25 13:27:46 -0700

    yeesh, added some buttons for operator functionality, not finished

--- src/data/OperatingHandler.java
+++ src/data/OperatingHandler.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import presentation.ManagementPage;
+import presentation.StorePage;
 import presentation.View;
 
 public class OperatingHandler implements Handler{
@@ -16,11 +17,33 @@ public class OperatingHandler implements Handler{
 
 	@Override
 	public void setupButtons(View view, Controller controller) {
+		
 		ManagementPage gui = (ManagementPage) view;
 		gui.backButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				controller.switchTo("Home");
+			}
+		});
+
+		gui.addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Add button pressed");
+			}
+		});
+		
+		gui.editButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("edit button pressed");
+			}
+		});
+		
+		gui.deleteButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("delete button pressed");
 			}
 		});
 	}
--- src/presentation/ManagementPage.java
+++ src/presentation/ManagementPage.java
@@ -3,23 +3,28 @@ package presentation;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class ManagementPage extends JPanel implements View{
 	public JButton backButton;	//probably shouldn't make these public?
+	public JButton addButton, editButton, deleteButton;
 	
 	public ManagementPage() {
 		// TODO Auto-generated constructor stub
-		JLabel test = new JLabel("Test");
+		JLabel test = new JLabel("This is the window\nfor viewing Documents,\nall of them");
 		setLayout(new BorderLayout());
 		
 		JPanel panel = new JPanel();
 		panel.add(test);
-		panel.add(backButton = new JButton("Back"));
+		
 		add(panel, BorderLayout.CENTER);
+		buttons();
 	}
 	@Override
 	public void display() {
@@ -39,6 +44,28 @@ public class ManagementPage extends JPanel implements View{
 	}
 	@Override
 	public void updateData(ArrayList<Object> arr) {
+	
+	}
+
+	private void buttons () {
+		JPanel botPan = new JPanel();
+		botPan.add(backButton = new JButton("Back"));
+
+		JPanel topPan = new JPanel();
 		
+		topPan.add(addButton = new JButton("Add"));
+		
+		topPan.add(editButton = new JButton("Edit"));
+		
+		topPan.add(deleteButton = new JButton("Delete"));
+			
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
+		
+		p.add(topPan);
+		p.add(new JSeparator(SwingConstants.HORIZONTAL));
+		p.add(botPan);
+		
+		add(p, BorderLayout.PAGE_END);
 	}
 }
